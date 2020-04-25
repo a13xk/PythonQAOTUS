08c219d0c3b45d2ea115e79c199f82c9446b8531
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-04-25 11:54:56 +0300

    Add GeometricFigure generic class

---
+++ Lesson6/geometric_figure.py
@@ -0,0 +1,32 @@
+class GeometricFigure:
+
+    def __init__(self, name: str, angles: int):
+        self.name: str = name
+        self.angles: int = angles
+    #
+
+    def area(self) -> float:
+        """
+        Area of geometric figure
+        """
+        return 0.0
+    #
+
+    def perimeter(self) -> float:
+        """
+        Perimeter of geometric figure
+        """
+        return 0.0
+    #
+
+    def add_area(self, figure):
+        """
+        Calculate area of 2 figures
+        :param figure: Object of GeometricFigure class
+        """
+        if type(figure) != GeometricFigure:
+            raise NameError(f"Invalid type of object {figure}")
+        return self.area() + figure.area()
+    #
+#
+

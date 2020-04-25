3d9158927b591eaeb5b3b38c6b67fc4ca745ec7b
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-04-25 12:18:15 +0300

    Implement Circle class

---
+++ Lesson6/circle.py
@@ -0,0 +1,31 @@
+from Lesson6.geometric_figure import GeometricFigure
+import math
+
+
+class Circle(GeometricFigure):
+
+    def __init__(self, name: str, **kwargs):
+        super().__init__(name=name, angles=0)
+
+        # Define circle by radius
+        if kwargs.get("radius"):
+            self.radius = kwargs.get("radius")  # Length of circle radius
+
+            if type(self.radius) != float:
+                raise ValueError(f"Invalid type of circle radius: {type(self.radius)}")
+            if self.radius <= 0.0:
+                raise ValueError(f"Invalid value of circle radius: {self.radius}")
+    #
+
+    def area(self) -> float:
+        return math.pi * (self.radius ** 2)
+    #
+
+    def perimeter(self) -> float:
+        return 2 * math.pi * self.radius
+    #
+
+    def add_area(self, figure):
+        super().add_area(figure=figure)
+    #
+#

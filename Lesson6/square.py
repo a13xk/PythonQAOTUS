9be3f762e50d2210fb736e059d06f7565c6b802a
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-04-25 12:07:46 +0300

    Implement Square class

---
+++ Lesson6/square.py
@@ -0,0 +1,28 @@
+from Lesson6.geometric_figure import GeometricFigure
+
+
+class Square(GeometricFigure):
+
+    def __init__(self, name: str, **kwargs):
+        super().__init__(name=name, angles=4)
+
+        # Define square by 1 side
+        if kwargs.get("side_a"):
+            self.side_a = kwargs.get("side_a")  # Length of side A
+
+            if type(self.side_a) != float:
+                raise ValueError(f"Invalid type of rectangle side A: {type(self.side_a)}")
+    #
+
+    def area(self) -> float:
+        return self.side_a * self.side_a
+    #
+
+    def perimeter(self) -> float:
+        return (self.side_a + self.side_a) * 2
+    #
+
+    def add_area(self, figure):
+        super().add_area(figure=figure)
+    #
+#

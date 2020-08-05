f73d102d2e71d831880db216eb654ec072850bdc
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-08-05 18:57:59 +0300

    Add 'list particular running process' switch

--- Lesson23/file_system_info.py
+++ Lesson23/file_system_info.py
@@ -10,12 +10,15 @@ class FileSystemInfo:
     def __init__(self, cli_arguments: argparse.Namespace):
         self.version: bool = cli_arguments.version
         self.show_all_processes: bool = cli_arguments.show_all_processes
+        self.process_id: int = cli_arguments.process_id
 
         self.res_all_processes: str = ""
+        self.res_process: str = ""
     #
 
     def __del__(self):
         self.res_all_processes = ""
+        self.res_process = ""
     #
 
     def print_version(self):
@@ -53,6 +56,22 @@ class FileSystemInfo:
         print("=== END ===\n")
         exit()
     #
+
+    def print_process_info(self):
+        """
+        Print information on specified process by its id
+        """
+        print(f"=== Information on process #{self.process_id} ===")
+        self.res_process = self.run_command_in_shell(command_args=[
+            "ps", "aux", "|",
+            "grep", str(self.process_id), "|",
+            "grep", "-v", "grep", "|",
+            "grep", "-v", f"{__file__}"
+        ])
+        print(self.res_process)
+        print("=== END ===\n")
+        exit()
+    #
 #
 
 
@@ -74,6 +93,13 @@ if __name__ == '__main__':
         action="store_true",
         help="list all running processes"
     )
+    arg_parser.add_argument(
+        "--process-id",
+        metavar="PID",
+        action="store",
+        type=int,
+        help="show information on particular process with specified PID"
+    )
     args = arg_parser.parse_args()
     args.func(args)
 
@@ -82,4 +108,6 @@ if __name__ == '__main__':
         fs.print_version()
     if fs.show_all_processes:
         fs.print_all_processes()
+    if fs.process_id:
+        fs.print_process_info()
 #

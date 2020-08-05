74594593f1830029c515400ea50ee7da59abcde9
Alexander Knyazev <26230870+a13xk@users.noreply.github.com>
2020-08-05 18:56:11 +0300

    Add 'list all running processes' switch

--- Lesson23/file_system_info.py
+++ Lesson23/file_system_info.py
@@ -9,6 +9,13 @@ class FileSystemInfo:
 
     def __init__(self, cli_arguments: argparse.Namespace):
         self.version: bool = cli_arguments.version
+        self.show_all_processes: bool = cli_arguments.show_all_processes
+
+        self.res_all_processes: str = ""
+    #
+
+    def __del__(self):
+        self.res_all_processes = ""
     #
 
     def print_version(self):
@@ -33,6 +40,19 @@ class FileSystemInfo:
         output = shell_process.stdout.rstrip()
         return output
     #
+
+    def print_all_processes(self):
+        """
+        Print all running processes' information from console
+        """
+        self.res_all_processes = self.run_command_in_shell(command_args=[
+            "ps", "aux"
+        ])
+        print("=== All processes ===")
+        print(self.res_all_processes)
+        print("=== END ===\n")
+        exit()
+    #
 #
 
 
@@ -49,6 +69,17 @@ if __name__ == '__main__':
         action="store_true",
         help="show version and exit"
     )
+    arg_parser.add_argument(
+        "--show-all-processes",
+        action="store_true",
+        help="list all running processes"
+    )
     args = arg_parser.parse_args()
     args.func(args)
+
+    fs = FileSystemInfo(cli_arguments=args)
+    if fs.version:
+        fs.print_version()
+    if fs.show_all_processes:
+        fs.print_all_processes()
 #
